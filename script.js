cd64d8763f7fe5039dadd6553c628ce9ebafe367
Beez <ayeoman41@gmail.com>
2022-12-09 16:34:32 -0500

    function to show questions added

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function no(){
     document.querySelector('.button')
     alert("Incorrect")
 }
+
+let questions =[
+    {
+        numb: 1,
+        question: "The Miami Hurrianes mascot is...?",
+        answer: "The Ibis",
+        options: [
+            "The Tiger",
+            "The Bear",
+            "The Panther",
+            "The Viking"
+        ]
+    }
+]
+
+function showQuestions(index){
+    const que = document.querySelector(".que");
+}
